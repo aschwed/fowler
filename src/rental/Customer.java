aa81802eb770a6b12f05c004c1d9ac5a59064adf
Anastasia Schwed <nshmyreva@gmail.com>
2018-05-22 20:23:30 +0200

    Fix smell: Switch statements should have a default label

--- src/rental/Customer.java
+++ src/rental/Customer.java
@@ -50,11 +50,6 @@ class Customer {
 	private double amountFor(Rental each) {
 		double thisAmount = 0;
 		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			break;
 		case Movie.NEW_RELEASE:
 			thisAmount += each.getDaysRented() * 3;
 			break;
@@ -62,6 +57,12 @@ class Customer {
 			thisAmount += 1.5;
 			if (each.getDaysRented() > 3)
 				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+		case Movie.REGULAR:
+		default:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
 			break;
 		}
 		return thisAmount;
