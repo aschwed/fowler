ed927dd94e87dda6a63d96b936ba4d2cba88d573
Anastasia Schwed <nshmyreva@gmail.com>
2018-05-22 20:42:29 +0200

    Use appropriate names for the method that generates a statement

--- src/rental/Customer.java
+++ src/rental/Customer.java
@@ -19,7 +19,7 @@ class Customer {
 		return name;
 	};
 
-	public String statement() {
+	public String generateCustomerStatement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		
@@ -47,24 +47,24 @@ class Customer {
 	}
 
 	private double calculateRentalAmount(Rental rental) {
-		double thisAmount = 0;
+		double amount = 0;
 		switch (rental.getMovie().getPriceCode()) {
 		case Movie.NEW_RELEASE:
-			thisAmount += rental.getDaysRented() * 3;
+			amount += rental.getDaysRented() * 3;
 			break;
 		case Movie.CHILDRENS:
-			thisAmount += 1.5;
+			amount += 1.5;
 			if (rental.getDaysRented() > 3)
-				thisAmount += (rental.getDaysRented() - 3) * 1.5;
+				amount += (rental.getDaysRented() - 3) * 1.5;
 			break;
 		case Movie.REGULAR:
 		default:
-			thisAmount += 2;
+			amount += 2;
 			if (rental.getDaysRented() > 2)
-				thisAmount += (rental.getDaysRented() - 2) * 1.5;
+				amount += (rental.getDaysRented() - 2) * 1.5;
 			break;
 		}
-		return thisAmount;
+		return amount;
 	}
 
 }
