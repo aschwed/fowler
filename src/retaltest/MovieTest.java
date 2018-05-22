f5aa0d035a1d954404718684738096713a855766
Anastasia Schwed <nshmyreva@gmail.com>
2018-05-22 21:19:58 +0200

    Unit tests for Movie

--- src/retaltest/MovieTest.java
+++ src/retaltest/MovieTest.java
@@ -1,5 +1,36 @@
 package retaltest;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import rental.Movie;
+
 public class MovieTest {
+
+	private final static String MOVIE_TITLE = "title";
+
+	private Movie codeUnderTest;
+
+	@Before
+	public void setUp() {
+		codeUnderTest = new Movie(MOVIE_TITLE, Movie.REGULAR);
+	}
+
+	@Test
+	public void testGetPriceCode() {
+		Assert.assertEquals(Movie.REGULAR, codeUnderTest.getPriceCode());
+	}
+
+	@Test
+	public void testSetPriceCode() {
+		codeUnderTest.setPriceCode(Movie.NEW_RELEASE);
+		Assert.assertEquals(Movie.NEW_RELEASE, codeUnderTest.getPriceCode());
+	}
+
+	@Test
+	public void testGetTitle() {
+		Assert.assertEquals(MOVIE_TITLE, codeUnderTest.getTitle());
+	}
 
 }
